622847b137e6eaded25c23cb36fa3588f39ef88a
Nazz <nazz.panasiuk@gmail.com>
2024-03-15 17:51:22 +0200

    Revert "read/update methods"
    This reverts commit 43675c05864432c1ba4b52e5879db6e0cbb544ee.

--- src/main/java/com/example/bankservice/exception/NullEntityReferenceException.java
+++
@@ -1,9 +0,0 @@
-package com.example.bankservice.exception;
-
-public class NullEntityReferenceException extends RuntimeException {
-    public NullEntityReferenceException() {    }
-
-    public NullEntityReferenceException(String message) {
-        super(message);
-    }
-}
